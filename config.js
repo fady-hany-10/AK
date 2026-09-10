29e50f54a514e1701a0874a83ca6a9d158cdccbe
fady hany <fadyhanyyy7@gmail.com>
2026-09-11 01:20:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 const config = {
-    title: "White for woman",
-    subtitle: " Welcome to my page",
+    title: "Ak",
+    subtitle: "social media",
     logo: "logo (3).png",
     links: [
         {
-            name: "Instagram",
-            url: "https://www.instagram.com/white.for.women?stkn=MTBhenAwZmtvOTR5dg==",
-            icon: "fa-brands fa-instagram",
-            class: "btn-instagram"
-        },
-        {
             name: "Facebook",
-            url: "https://www.facebook.com/share/1CSEAmQhSR/?mibextid=wwXIfr",
+            url: "https://www.facebook.com/share/1BCCNQcUNm/",
             icon: "fa-brands fa-facebook-f",
             class: "btn-facebook"
         },
         {
+            name: "Instagram",
+            url: "https://www.instagram.com/ak_mens_collection?stkn=MmNuZzY2cXh3dnhp",
+            icon: "fa-brands fa-instagram",
+            class: "btn-instagram"
+        },
+        {
             name: "TikTok",
-            url: "https://www.tiktok.com/@white.for.women?_r=1&_t=ZS-99bIeLph8FN",
+            url: "https://www.tiktok.com/@ak.menscollection?_r=1&_t=ZS-99cZOFO0RAT",
             icon: "fa-brands fa-tiktok",
             class: "btn-tiktok"
         },
         {
-            name: "InstaPay",
-            url: "https://ipn.eg/S/moazkishk/instapay/3NfHks",
-            icon: "fa-solid fa-wallet",
-            class: "btn-instapay"
+            name: "Vodafone Cash",
+            url: "tel:01063898192",
+            icon: "fa-solid fa-mobile-screen-button",
+            class: "btn-vodafone"
         }
     ]
 };
